0105fd47da44b7340bdde015b0225d61acbf71d8
felixtomlinson <felix.tomlinson@faststream.civilservice.gov.uk>
2017-12-15 13:05:05 +0000

    Updating Script reader to improve the scene number detection.

--- Script_reader.py
+++ Script_reader.py
@@ -18,49 +18,6 @@ def document_reader(file):
     text = text.splitlines()
     return text
 
-def scene_numberer(script_as_list, index):
-    '''Searches the area surrounding the key text to see if there are places where scene numbers that might be\
-it then checks to see if they are anywhere else and returns those numbers if they are'''
-    script_length = len(script_as_list)
-    if (index-2) > 0:
-        if (index+2) < script_length:
-            if script_as_list[index-2] == script_as_list[index+2]:
-                return script_as_list[index+2]
-            elif (index+1) == '':
-                scene_number = index+2
-                for strings in script_as_list[index-10:index]:
-                    there_or_not = strings.find(scene_number)
-                    if there_or_not != -1:
-                        return scene_number
-            elif (index-1) == '':
-                scene_number = index-2
-                for strings in script_as_list[index:index+10]:
-                    there_or_not = strings.find(scene_number)
-                    if there_or_not != -1:
-                        return scene_number
-            else:
-                short_strings_within5 = []
-                short_strings_within10 = []
-                for strings in script_as_list[index-5:index+5]:
-                    if len(strings) <= 3:
-                        if len (strings) > 0:
-                            if not '.' in strings:
-                                short_strings_within5.append(strings)
-                for short_strings in short_strings_within5:
-                    number_of_occurences = script_as_list[index-5:index+5].count(short_strings)
-                    if number_of_occurences == 2:
-                        return short_strings
-                for strings in script_as_list[index-10:index+10]:
-                    if len(strings) <= 3:
-                        if len (strings) > 0:
-                            if not '.' in strings:
-                                short_strings_within10.append(strings)
-                for short_strings in short_strings_within10:
-                    number_of_occurences = script_as_list[index-10:index+10].count(short_strings)
-                    if number_of_occurences == 2:
-                        return short_strings
-    return ''
-
 def text_splitter (important_text):
     '''Splits the various important parts of the text out into a list. The important parts for this tool are: \
 if the location is inside or outside, the location details and what time of day the actions is happening at'''
@@ -99,13 +56,6 @@ def heading_decider(output_type):
     else:
         return (['Scene Number','Internal or external','Type of Location','Time of Day'])
 
-def line_generator(list, output_type):
-    '''Turns a list with three objects in it either into CSV or a table depending on the ouput type selected'''
-    if output_type == 'CSV':
-        return list[0] + ',' + list[1] + ',' + list[2] + ',' + list[3] + '\n'
-    else:
-        return list
-
 def important_text_compiler(list_of_strings, string, index):
     inside_or_out_or_both = ['INT./EXT.', 'INT.', 'EXT.']
     for category in inside_or_out_or_both:
@@ -115,21 +65,126 @@ def important_text_compiler(list_of_strings, string, index):
             return new_string
     return string
 
+def rough_scene_checker(script_as_list, index, which_side):
+    if len(script_as_list[index + 2 * which_side]) <= 5:
+        if len (script_as_list[index + 2 * which_side]) > 0:
+            if not '.' in script_as_list[index + 2 * which_side]:
+                scene_number = script_as_list[index + 2 * which_side]
+                return scene_number
+
+def potential_scene_number_compiler(script_as_list):
+    potential_strings_within_range = []
+    for strings in script_as_list:
+        if len(strings) <= 5:
+            if len (strings) > 0:
+                if not '.' in strings:
+                    potential_strings_within_range.append(strings)
+    return potential_strings_within_range
+
+def scene_number_finder(script_as_list):
+    upper_range_number = potential_scene_number_compiler(script_as_list[1])
+    lower_range_number = potential_scene_number_compiler(script_as_list[0])
+    if bool(set(upper_range_number) & set(lower_range_number)):
+        for numbers in upper_range_number:
+            upper_number = numbers
+            for scene_numbers in lower_range_number:
+                if scene_numbers == upper_number:
+                    return scene_numbers
+
+def scene_number_finder_extreme(script_as_list):
+    upper_range_number = potential_scene_number_compiler(script_as_list[1])
+    lower_range_number = potential_scene_number_compiler(script_as_list[0])
+    searchable_script_as_list = []
+    for string in script_as_list[0]:
+        string = string.split()
+        searchable_script_as_list.extend(string)
+    for string in script_as_list[1]:
+        string = string.split()
+        searchable_script_as_list.extend(string)
+    for short_strings in upper_range_number:
+        number_of_occurences = searchable_script_as_list.count(short_strings)
+        if number_of_occurences == 2:
+            return short_strings
+
+def script_trimmer(script_as_list, script_length, index, search_range):
+    lower_index = index - search_range
+    upper_index = index + search_range
+    if lower_index < 0:
+        lower_index = 0
+    if upper_index > script_length:
+        upper_index = script_length
+    lower_half = script_as_list[lower_index:index]
+    upper_half = script_as_list[index:upper_index]
+    return [lower_half, upper_half]
+
+def for_looper(script_as_list, script_length, index, start, end):
+    for number in range(start, end):
+        trimmed_script = script_trimmer(script_as_list, script_length, index, number)
+        scene_number = scene_number_finder(trimmed_script)
+        if scene_number != None:
+            #print (scene_number + ' - Needed to search as far as: '+str(number)+'! Range = ' + str(start) + ',' + str(end))
+            return scene_number
+
+def extreme_for_looper(script_as_list, script_length, index, start, end):
+    for number in range(start, end):
+        trimmed_script = script_trimmer(script_as_list, script_length, index, number)
+        scene_number = scene_number_finder_extreme(trimmed_script)
+        if scene_number != None:
+            #print (scene_number + ' - Needed an extreme search as far as: '+str(number)+'! Range = ' + str(start) + ',' + str(end))
+            return scene_number
+
+def scene_numberer(script_as_list, index):
+    '''Searches the area surrounding the key text to see if there are places where scene numbers that might be\
+it then checks to see if they are anywhere else and returns those numbers if they are'''
+    script_length = len(script_as_list)
+    if script_as_list[index-2] == script_as_list[index+2]:
+        if (index-2) > 0:
+            if (index+2) < script_length:
+                return script_as_list[index+2]
+    lowers = [3, 9, 13]
+    uppers = [8, 12, 16]
+    combined = zip(lowers,uppers)
+    for i, j in combined:
+        scene_number = for_looper(script_as_list, script_length, index, i, j)
+        if scene_number != None:
+            return scene_number
+        scene_number = extreme_for_looper(script_as_list, script_length, index, i, j)
+        if scene_number != None:
+            return scene_number
+    if (script_as_list[index+1]) == '' or (script_as_list[index-1]) == '':
+        scene_number = rough_scene_checker(script_as_list, index, 1)
+        if scene_number == None:
+            scene_number = rough_scene_checker(script_as_list, index, -1)
+        if scene_number != None:
+            #print (scene_number + ' - Used the rough scene checker!')
+            return scene_number
+    #print ('Try again', index, trimmed_script)
+    return ''
+
+def line_generator(list, output_type, index_list):
+    '''Turns a list with three objects in it either into CSV or a table depending on the ouput type selected'''
+    if output_type == 'CSV':
+        return list[0] + ',' + list[1] + ',' + list[2] + ',' + list[3] + '\n'
+    else:
+        return list
+
 def table_creator(script):
     '''This function takes the script as an input and uses the formatted_lines function to add all the locations together and\
  turns it in into a table.'''
     script = document_reader(script)
     table = PrettyTable(heading_decider('text'))
     index = 0
+
     for lines in script:
         formatted_lines = text_splitter(lines)
         if formatted_lines != None:
             if formatted_lines[1]=='':
-                compiled_line = important_text_compiler(script, lines, index )
+                compiled_line = important_text_compiler(script, lines, index)
                 formatted_lines = text_splitter(compiled_line)
             scene_number = scene_numberer(script, index)
             formatted_lines.insert(0, scene_number)
-            table.add_row(line_generator(formatted_lines, 'text'))
+
+            table.add_row(line_generator(formatted_lines, 'text', index_list))
         index += 1
     return str(table)
 
@@ -138,6 +193,7 @@ def file_namer(file):
     return ('Location Information for '+ os.path.splitext(file)[0] + '.csv')
 
 def csv_creator(script):
+    #CHECK THIS AS IT PROBABLY NO LONGER WORKS
     '''This function takes the script as an input and uses the formatted_lines function to add all the locations together and\
     creates a CSV file.'''
     file_name = file_namer(script)
